d57cabb55d653d0cde73a6540fb8cb838a5cd009
catperez81 <cat@healthsherpa.com>
2015-09-07 15:15:43 -0700

    update js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,19 @@ $(document) .ready(function() {
 	.mouseleave(function() {
 		$('.ryu-still').show();
 		$('.ryu-ready').hide();
-	});
+	})
+	.mousedown(function() {
+		//play hadouken sound
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		//show hadouken and animate it to the right of the screen
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+		//ryu goes back to his ready position
+	})
 
 });
 
