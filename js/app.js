542daf9f5c136c2346cc637e8161ccf94db5c76c
catperez81 <cat@healthsherpa.com>
2015-09-07 15:06:32 -0700

    update js and css

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
-$( ".ryu-ready") .hide();
-$( ".ryu-throwing") .show();
+$(document) .ready(function() {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();	
+	})
+	.mouseleave(function() {
+		$('.ryu-still').show();
+		$('.ryu-ready').hide();
+	});
+
+});
+
