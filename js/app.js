2d0a19610173a4084787d9dda129ad47838a8cff
catperez81 <cat@healthsherpa.com>
2015-09-07 15:35:03 -0700

    update js and html with audio

--- js/app.js
+++ js/app.js
@@ -8,17 +8,27 @@ $(document) .ready(function() {
 		$('.ryu-ready').hide();
 	})
 	.mousedown(function() {
-		//play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		//show hadouken and animate it to the right of the screen
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			});
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-		//ryu goes back to his ready position
-	})
+	});
 
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume=0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
 
